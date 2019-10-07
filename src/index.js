4eb0fdab5343b87843690ddb3399e63c763df9a7
Liudmila Arzamasova <liudmila.arzamasova@gmail.com>
2019-10-08 00:03:10 +0300

    feat: found count zeros of factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-module.exports = function zeros(expression) {
-  // your solution
-}
+module.exports = expression => {
+  let arrNumbers = [];
+
+  expression.split("*").forEach(elem => {
+    let result = BigInt(1);
+    let num = BigInt(parseInt(elem));
+
+    if (parseInt(elem) % 2 === 0 && elem.includes("!!")) {
+      while (num) {
+        result *= num;
+        num -= BigInt(2);
+      }
+
+      arrNumbers.push(result);
+      return;
+    }
+
+    if (parseInt(elem) % 2 !== 0 && elem.includes("!!")) {
+      while (num - BigInt(1) !== BigInt(0)) {
+        result *= num;
+        num -= BigInt(2);
+      }
+
+      arrNumbers.push(result);
+      return;
+    }
+
+    while (num - BigInt(1) !== BigInt(0)) {
+      result *= num;
+      num -= BigInt(1);
+    }
+    arrNumbers.push(result);
+    return;
+  });
+
+  const result = arrNumbers.reduce((acc, next) => BigInt(acc) * BigInt(next));
+
+  let countZeros = 0;
+  const numbers = String(result)
+    .split("")
+    .reverse();
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== "0") {
+      break;
+    }
+    countZeros++;
+  }
+
+  return countZeros;
+};
+
